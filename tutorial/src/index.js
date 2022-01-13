b7d9832427f09ac8db2a5106a3f4f7a60c2859be
Lanre Sam <ezechelanre@gmail.com>
2022-01-13 21:51:34 +0100

    writing code

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Name() {
-    return <h4>My name is Lanre</h4>;
+    return 
+    <h4>
+        My name is Lanre
+    </h4>;
 }
 
 ReactDom.render(<Name />, document.getElementById('root'));
+
