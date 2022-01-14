3c990c1d4829ef348c88512fd06f4711055e94e6
Lanre Sam <ezechelanre@gmail.com>
2022-01-14 12:22:31 +0100

    building components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 
 import ReactDom from 'react-dom';
 
-function Name() {
-    return 
-    <h4>
-        My name is Lanre
-    </h4>;
+// function Name() {
+//     return (
+//     <h4>
+//         My name is Lanre
+//     </h4>);
+// }
+
+function App () {
+    return (
+        <div>
+            <Name/>
+            <Message/>
+        </div>
+    );
 }
 
-ReactDom.render(<Name />, document.getElementById('root'));
+const Name = () => <h2>Lanre</h2>;
+const Message = () => { return <p>Lorem ipsum dolor, 
+                sit amet consectetur adipisicing elit. Ex molestiae aliquid soluta dolorem a dolorum necessitatibus 
+                et suscipit dolor aut.
+            </p>};
+
+
+ReactDom.render(<App />, document.getElementById('root'));
 
